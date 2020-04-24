cf16721aa2e81b6da778ce759a3f3cd954bb2a02
James Moessis <james.moessis1@gmail.com>
2020-04-24 21:01:12 +1000

    Inject audio into volumetric particle system, and tweaks

---
+++ Assets/Scripts/VolumetricAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumetricAudio : MonoBehaviour {
+    public Audio source;
+    public float maxChange;
+    public float multiplier;
+
+    private UnityEngine.VFX.VisualEffect visualEffect;
+    private float prevValue;
+    
+
+    void Start() {
+        visualEffect = this.GetComponent<UnityEngine.VFX.VisualEffect>();
+        prevValue = 0;
+    }
+
+    void Update() {
+        float audioValue = Audio._lowerAmplitude;
+        visualEffect.SetFloat("AudioLevel", audioValue*multiplier);
+    }
+}
