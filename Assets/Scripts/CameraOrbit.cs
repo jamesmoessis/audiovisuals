d5f0f222c2d3e38e811396bd53cb1122ff80192f
James Moessis <james.moessis1@gmail.com>
2020-05-04 20:27:49 +1000

    camera terrain orbit script, primitive gradient switch on trebles

---
+++ Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour {
+    
+    public GameObject Turret;//to get the position in worldspace to which this gameObject will rotate around.
+
+    [Header("The axis by which it will rotate around")]
+    public Vector3 axis;//by which axis it will rotate. x,y or z.
+
+    [Header("Angle covered per update")]
+    public float angle; //or the speed of rotation.
+
+
+    void Update() {
+        //Gets the position of your 'Turret' and rotates this gameObject around it by the 'axis' provided at speed 'angle' in degrees per update 
+        transform.RotateAround(Turret.transform.position, axis, angle);
+        transform.position = new Vector3(transform.position.x, 2 + Mathf.Sin(0.8f*Time.time), transform.position.z);
+        angle = Triangle(-0.4f, 0.4f, 50, 0, Time.time);
+    }
+
+    float Triangle(float minLevel, float maxLevel, float period, float phase, float t) {
+        float pos = Mathf.Repeat(t - phase, period) / period;
+
+        if (pos < .5f) {
+            return Mathf.Lerp(minLevel, maxLevel, pos * 2f);
+        } else {
+            return Mathf.Lerp(maxLevel, minLevel, (pos - .5f) * 2f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/TerrainMusic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainMusic : MonoBehaviour {
+    public Audio source;
+    private Vector3 audioValues;
+    private UnityEngine.VFX.VisualEffect visualEffect;
+    private Gradient startGradient;
+    public Gradient highsGradient;
+
+
+    void Start() {
+        visualEffect = this.GetComponent<UnityEngine.VFX.VisualEffect>();
+        this.startGradient = visualEffect.GetGradient("HeightFieldColorMap");
+        audioValues = new Vector3(0, 0, 0);
+        Debug.Log(startGradient.colorKeys);
+    }
+
+    void Update() {
+        audioValues.x = source._lowerAmplitude;
+        audioValues.y = source._amplitude;
+        audioValues.z = source._higherAmplitude;
+        if (audioValues.z > 0.8) {
+            visualEffect.SetGradient("HeightFieldColorMap", highsGradient);
+        } else {
+            visualEffect.SetGradient("HeightFieldColorMap", startGradient);
+        }
+        visualEffect.SetVector3("AudioLevels", audioValues);
+    }
+}
